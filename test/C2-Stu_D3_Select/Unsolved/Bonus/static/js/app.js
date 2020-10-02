9ae9a6babcaea727ea0c67bac60461379acc55f4
sfnxboy <amiramire12@gmail.com>
2020-10-01 20:12:54 -0400

    test

--- test/C2-Stu_D3_Select/Unsolved/Bonus/static/js/app.js
+++
@@ -1,9 +0,0 @@
-// Use D3 to select the table body
-
-// Use D3 to select the table
-
-// Use D3 to set the table class to `table table-striped`
-
-// BONUS: Dynamic table
-// Loop through an array of grades and build the entire table body from scratch
-let grades = [["Malcolm", 80], ["Zoe", 85], ["Kaylee", 99], ["Simon", 99], ["Wash", 79]];
